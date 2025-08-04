f43354b5e156da0f76017a30b924d2dacf66daea
Cactric <59334627+Cactric@users.noreply.github.com>
2025-08-04 14:30:16 +0100

    Print latency

--- src/lib.rs
+++ src/lib.rs
@@ -47,10 +47,10 @@ pub fn send_ping(host_info: &mut HostInfo, socket: &Socket) -> Result<(), Error>
     // Fill the buffer with the system time, then the numbers 0x10 to 0x37
     // (this is to mimic the packets of the ping(8) command)
     let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
-    let secs = time.as_secs().to_be();
-    let nanos = (time.subsec_nanos() as u64 / 1000).to_be();
+    let secs = time.as_secs();
+    let micros = (time.subsec_nanos() as u64 / 1000);
     let mut buf: Vec<u8> = construct_echo_request(0xbeef, 1, &secs.to_be_bytes());
-    buf.append(&mut nanos.to_be_bytes().to_vec());
+    buf.append(&mut micros.to_be_bytes().to_vec());
     buf.append(&mut ((0x10 as u8)..=(0x37 as u8)).collect());
     socket.send_to(&buf, &host_info.host.into())?;
     return Ok(())
@@ -68,6 +68,19 @@ pub fn receive_ping(host_info: &HostInfo, mut socket: &Socket) -> Result<(), Err
             let maybe_message: Result<ICMPv4Message, IntoICMPv4MessageError> = rec_buf[..used_bytes].try_into();
             if let Ok(message) = maybe_message {
                 println!("Message received: {:?}", message);
+                // try to parse the timestamp in the received packet
+                let ts_seconds = u64::from_be_bytes(message.icmpv4_data[0..8].try_into().unwrap());
+                let ts_sub_micros = u64::from_be_bytes(message.icmpv4_data[8..16].try_into().unwrap());
+                println!("ts_seconds: {}, ts_sub_micros: {}", ts_seconds, ts_sub_micros);
+                let ts_micros = (ts_seconds as u128 * 1000000) + ts_sub_micros as u128;
+                
+                let cur_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+                let cur_micros = cur_time.as_nanos() / 1000;
+                
+                println!("ts_micros: {}, cur_micros: {}", ts_micros, cur_micros);
+                let diff_micros = cur_micros - ts_micros;
+                println!("Latency: {}s", diff_micros as f64 / 1000000.0);
+                
             } else if let Err(e) = maybe_message {
                 print!("Error parsing response: ");
                 match e {
